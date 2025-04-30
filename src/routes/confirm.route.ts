c7a12d04f6b4cab0439d7a4fd2960d60afb9b989
RBxRobson <robsonnbarbosa0@gmail.com>
2025-04-30 16:49:05 -0300

    feat: Adiciona arquivo de configuração da rota confirm

---
+++ src/routes/confirm.route.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client';
+import { Router } from 'express';
+import { confirmController } from '../controllers/confirm.controller';
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.patch('/confirm', confirmController(prisma));
+
+export default router;
